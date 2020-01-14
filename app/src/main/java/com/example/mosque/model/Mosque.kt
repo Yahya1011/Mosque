34d2c5e4441d69734865e8589935d22c4ae7b0ca
Yahya1011 <yahyaabdullah1011@gmail.com>
2020-01-14 13:25:31 +0700

    First

---
+++ app/src/main/java/com/example/mosque/model/Mosque.kt
@@ -0,0 +1,61 @@
+package com.example.mosque.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Mosque (
+    @SerializedName("id")
+    val id : Int,
+    @SerializedName("type")
+    val type : String,
+    @SerializedName("code")
+    val code : String,
+    @SerializedName("name")
+    val name : String,
+    @SerializedName("identity")
+    val identity : String,
+    @SerializedName("surface_area")
+    val surface_area : String,
+    @SerializedName("building_area")
+    val building_area : String,
+    @SerializedName("los")
+    val los : String,
+    @SerializedName("since")
+    val since : Int,
+    @SerializedName("bank_id")
+    val bank_id : Int,
+    @SerializedName("rek")
+    val rek : String,
+    @SerializedName("address")
+    val address : String,
+    @SerializedName("latitude")
+    val latitude : String,
+    @SerializedName("longitude")
+    val longitude : String,
+    @SerializedName("province_id")
+    val province_id : Int,
+    @SerializedName("estimate")
+    val estimate : String,
+    @SerializedName("estimate_date")
+    val estimate_date : String,
+    @SerializedName("city_id")
+    val city_id : Int,
+    @SerializedName("kec_id")
+    val kec_id : Int,
+    @SerializedName("kel_id")
+    val kel_id : String,
+    @SerializedName("pic")
+    val pic : String,
+    @SerializedName("description")
+    val description : String,
+    @SerializedName("bank")
+    val bank : Bank,
+    @SerializedName("province")
+    val province : Province,
+    @SerializedName("regency")
+    val regency : String,
+    @SerializedName("districts")
+    val districts : String,
+    @SerializedName("village")
+    val village : String
+
+)
